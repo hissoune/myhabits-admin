25b4dcb923ef569c4a95893b14a310470f2dba00
hissoune <khalidhissoune962@gmail.com>
2025-02-03 09:58:32 +0100

    [Admin] chore: Updated Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "v0.blob.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
