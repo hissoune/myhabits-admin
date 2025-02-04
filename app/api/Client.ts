a305ca74497d06c7795a2b4a77b9db4ed9d5c187
hissoune <khalidhissoune962@gmail.com>
2025-02-04 13:52:18 +0100

    [API] fix: Updated Client API logic

--- app/api/Client.ts
+++ app/api/Client.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import { cookies } from "next/headers"; 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.8.191:3000/',
+  baseURL: 'http://localhost:3000/',
 });
 
 axiosInstance.interceptors.request.use(
  async (config) => {
-    const token = (await cookies()).get("token")?.value; 
+    const token = (await cookies()).get("auth_token")?.value; 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
       config.headers["Content-Type"]=["application/json"]
