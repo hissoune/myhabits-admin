e13856b06eacceda2fe4507b97ec8f25291db9ef
hissoune <khalidhissoune962@gmail.com>
2025-02-04 13:52:19 +0100

    [Middleware] feat: Added global middleware for auth handling

---
+++ middleware.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from "next/server";
+import axiosInstance from "./app/api/Client";
+
+
+export async function middleware(req: NextRequest) {
+
+
+    const token = req.cookies.get("auth_token")?.value;
+  
+    const unauthRoutes = ["/login", "/register"];
+    const adminRoutes = ["/dashboard"];
+
+
+    
+    if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && !token) {
+        return NextResponse.next();
+      }
+      if (!token) {
+        return NextResponse.redirect(new URL("/login", req.url));
+      }
+      if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && token) {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+
+      
+    
+      const response = await axiosInstance.get(`auth/verify`);
+      
+      if (response.status == 401) {
+        return NextResponse.redirect(new URL("/login", req.url));
+
+      }
+    const decoded = response.data;
+
+    if (adminRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && decoded.role != "admin") {
+        return NextResponse.redirect(new URL("/unauthozized", req.url));
+      }
+     
+ const res = NextResponse.next();
+ res.cookies.set("user_data", JSON.stringify(decoded));
+ return res;
+
+   
+
+}
+
+export const config = {
+    matcher: [
+     
+      "/dashboard",
+      "/login",
+      "/register",
+    ],
+  };
