ea6171d329f3f1296bb3b13d8590f0d213dddf54
hissoune <khalidhissoune962@gmail.com>
2025-02-27 19:03:25 +0000

    Updated Client API service for better request handling

--- app/api/Client.ts
+++ app/api/Client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { cookies } from "next/headers"; 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: process.env.BASE_URL,
 });
 
 axiosInstance.interceptors.request.use(
