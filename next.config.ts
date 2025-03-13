b22bf615f3dc0b7623bb1bd8926b3e5e474f3969
hissoune <khalidhissoune962@gmail.com>
2025-03-13 23:40:48 +0000

    Updated Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+import { hostname } from "os"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.pinimg.com"],
+    domains: ["i.pinimg.com","127.0.0.1"],
   },
 }
 
