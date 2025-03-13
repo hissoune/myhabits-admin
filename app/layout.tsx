2b4b949cd49003666ac2f66f87b8e91c41bc99d1
hissoune <khalidhissoune962@gmail.com>
2025-03-13 22:46:46 +0000

    Updated layout file.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,13 +23,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+ 
+  
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main className="h-screen">
-         {children}
+        <Providers>
+
+          {children}
+        </Providers>
 
         </main>
 
