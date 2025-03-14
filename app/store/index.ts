6f2d48e42f940bb49a6b59d8c11f5154e54634da
hissoune <khalidhissoune962@gmail.com>
2025-03-14 15:13:44 +0000

    [UPDATE] Update index.ts for store configuration

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './slices/authSlice';
 import { habitsReducer } from './slices/habitsSlice';
+import { chalengReducer } from './slices/chalengesSlice';
 
 export const store = configureStore({
   reducer: {
      auth:authReducer,
-     habits:habitsReducer
+     habits:habitsReducer,
+     chalenges:chalengReducer
   },
 });
 
