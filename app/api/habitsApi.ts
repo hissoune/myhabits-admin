826ab82278b7257b505cb9597ec82db36baed269
hissoune <khalidhissoune962@gmail.com>
2025-03-14 20:48:00 +0000

    Update habitsApi.ts

--- app/api/habitsApi.ts
+++ app/api/habitsApi.ts
@@ -6,3 +6,14 @@ export const getAllHabits = async ()=>{
  const response = await axiosInstance.get('habits-service/habits/all_for_admin');
  return response.data
 }
+
+export const deleteHabit = async (habitId:string)=>{
+    const response = await axiosInstance.delete(`habits-service/habits/${habitId}`);
+    return response.data
+}
+
+
+export const reActiveHabit =async (habitId:string)=>{
+    const response = await axiosInstance.patch(`habits-service/habits/${habitId}`);
+    return response.data
+}
