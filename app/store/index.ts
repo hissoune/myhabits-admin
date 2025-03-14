3bc9aa5d23359633e986a5d3ac173fb364dc7f7e
hissoune <khalidhissoune962@gmail.com>
2025-03-14 11:40:09 +0000

    Updated Redux store index.

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './slices/authSlice';
+import { habitsReducer } from './slices/habitsSlice';
 
 export const store = configureStore({
   reducer: {
-     auth:authReducer
+     auth:authReducer,
+     habits:habitsReducer
   },
 });
 
