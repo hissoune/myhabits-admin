394a8cee575ea7cb5ef532868793148d356a6756
hissoune <khalidhissoune962@gmail.com>
2025-03-14 11:40:09 +0000

    Updated types definition.

--- types.ts
+++ types.ts
@@ -7,3 +7,33 @@ export type User ={
     image:string
     role?:string
 }
+
+export enum Frequency {
+    Daily = 'daily',
+    Weekly = 'weekly',
+    Monthly = 'monthly'
+}
+
+export enum Status {
+    Active = 'active',
+    Completed = 'completed',
+    Failed = 'failed'
+}
+
+
+
+export type Habit=  {
+    _id?:string
+    userId?: User;
+    progress?: number;
+    title: string;
+    description?: string;
+    frequency: Frequency;
+    reminderTime?: Date;
+    repeats:number;
+    sucsess?:number;
+    fails?:number;
+    status?: Status;
+    createdAt: Date;
+    updatedAt: Date;
+}
