8f5581239b1f5afc9d9c82040cdd65ca25e5e2b9
hissoune <khalidhissoune962@gmail.com>
2025-03-15 23:20:49 +0000

    Add jest.config.js for Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testEnvironment: 'node', 
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/$1',
+    },
+  };
