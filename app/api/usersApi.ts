82307db4068adc2bb5bffc1e03cedded7edbb2df
hissoune <khalidhissoune962@gmail.com>
2025-03-15 20:48:01 +0000

    Update usersApi.ts for enhanced user management API integration

--- app/api/usersApi.ts
+++ app/api/usersApi.ts
@@ -6,3 +6,8 @@ export const getAllUsers = async ()=>{
    const response = await axiosInstance.get('auth-service/auth/all_users');
    return response.data
 }
+
+export const banOrUnban = async (userId:string)=>{
+   const response = await axiosInstance.patch(`auth-service/auth/userActivity/${userId}`);
+   return response.data
+}
