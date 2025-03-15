d005c0fff58fce3a5ed00080096de6d5f813b48f
hissoune <khalidhissoune962@gmail.com>
2025-03-15 17:41:54 +0000

    Update chalengeApi.ts to support delete, create, and update challenge functionality

--- app/api/chalengeApi.ts
+++ app/api/chalengeApi.ts
@@ -1,3 +1,4 @@
+import { chalenge } from "@/types";
 import axiosInstance from "./Client"
 
 
@@ -5,4 +6,16 @@ import axiosInstance from "./Client"
 export const getAllChalenges = async ()=>{
 const response = await axiosInstance.get('chalenges-service/chalenges');
 return response.data
+}
+
+export  const createChallenge = async (chalenge:chalenge)=>{
+    const response = await axiosInstance.post('chalenges-service/chalenges',chalenge);
+    return response.data
+}
+
+export const deletChalenge =async (chalengeId:string)=>{
+    const response = await axiosInstance.delete(`chalenges-service/chalenges/${chalengeId}`);
+    console.log();
+    
+    return response.data
 }
